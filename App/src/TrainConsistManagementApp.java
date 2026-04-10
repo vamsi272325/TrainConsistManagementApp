042bdf414f3957a0039f0e6938d90719a89ccf29
vamsi272325 <rr09368053357@gmail.com>
2026-04-10 14:08:50 +0530

    uc1

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+    public class TrainConsistManagementApp {
+
+        public static void main(String[] args) {
+
+            System.out.println("=== Train Consist Management App ===");
+
+            List<String> trainConsist = new ArrayList<>();
+
+            System.out.println("Train consist initialized.");
+            System.out.println("Initial number of bogies: " + trainConsist.size());
+        }
+    }
+
